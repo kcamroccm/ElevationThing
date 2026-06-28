de06174e06a2d2e3ddfc17cf1885577a81170800
TMM <secret@notreal.com>
2026-06-28 15:42:53 -0400

    removed boilerplate notices and updated LICENSE

--- Source/ElevationThing/Private/ElevationThing.cpp
+++ Source/ElevationThing/Private/ElevationThing.cpp
@@ -1,5 +1,3 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "ElevationThing.h"
 
 #define LOCTEXT_NAMESPACE "FElevationThingModule"
--- Source/ElevationThing/Public/ElevationThing.h
+++ Source/ElevationThing/Public/ElevationThing.h
@@ -1,9 +1,6 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
-
 #include "Modules/ModuleManager.h"
-
 class FElevationThingModule : public IModuleInterface
 {
 public:
